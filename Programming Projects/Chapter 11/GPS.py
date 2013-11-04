aea1a1b95756a23806bd0939c93d2cf03af01704
MarcusG <marcus.greathouse@cs.olivetcollege.edu>
2013-11-03 21:40:49 -0500

    Expanding UI, tweaking framework

--- Programming Projects/Chapter 11/GPS.py
+++ Programming Projects/Chapter 11/GPS.py
@@ -13,11 +13,25 @@ class GPS(object):
         # as we cannot get the actual location (which would always be the same for a desktop), this is random
         lat = random.random() * 180 - 90
         lon = random.random() * 360 - 180
-        return (lat, lon)
+
+        wp = Waypoint(lat, lon)
+        return (wp)
 
     # get waypoint and return it
     def getWaypoint(self, waypoint):
-        return self.waypoints(waypoint)
+        return self.waypoints[waypoint]
+
+    # get waypoints and return them
+    def getWaypoints(self):
+        return self.waypoints
+
+    # returns path
+    def getPath(self, path):
+        return self.paths[path]
+
+    # returns paths
+    def getPaths(self):
+        return self.paths
 
     # returns number of waypoints
     def waypointsSaved(self):
@@ -27,6 +41,26 @@ class GPS(object):
     def pathsSaved(self):
         return len(self.paths.keys())
 
+    # creates a new path
+    def savePath(self, name):
+        self.paths[name] = Path()
+
+    # adds point to path, creates both if they do not exist
+    def addPoint(self, path, point):
+        if not path in self.paths.keys():
+            self.savePath(path)
+
+        if not point in self.waypoints.keys():
+            self.saveWaypoint(point)
+
+        # add the point to the path
+        self.paths[path].addWaypoint(point)
+
+    # removes all instances of a point in given path
+    def removePoint(self, path, point):
+        if path in self.paths.keys() and point in self.waypoints.keys():
+            self.paths[path].removeWaypoint(point)
+
     # saves the coordinates at a waypoint called the passed name
     def saveWaypoint(self, name):
         self.waypoints[name] = self.getLoc()
@@ -34,15 +68,16 @@ class GPS(object):
     # calculates distance between two points
     def distance(self, point1, point2=None):
         '''Decided to make this return real distance, borrowed code from stackoverflow'''
-        if point2 == None:
-            point2 = self.getLoc()
-        else:
-            point2 = self.waypoints[point2]
+        point1 = self.getWaypoint(point1)
 
-        point1 = self.waypoints[point1]
+        if point2 in self.waypoints.keys():
+            point2 = self.getWaypoint(point2)
+        else:
+            point2 = self.getLoc()
 
         # convert to radians
-        lon1, lat1, lon2, lat2 = map(radians, [point1[1], point1[0], point2[1], point2[0]])
+        lon1, lat1, lon2, lat2 = map(radians,
+            [point1.getLon(), point1.getLat(), point2.getLon(), point2.getLat()])
 
         # haversine formula
         dlon = lon1 - lon2
@@ -50,3 +85,70 @@ class GPS(object):
         a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
         c = 2 * asin(sqrt(a))
         return 6367 * c  # kilometers
+
+    # calculates the total distance for a path
+    def pathDistance(self, name):
+        distance = 0
+        if name in self.paths.keys():
+            pairs = self.paths[name].getPathPairs()
+
+            for pair in pairs:
+                distance += self.distance(pair[0], pair[1])
+
+        return distance
+
+    # replaces default identifier when used as a string
+    def __str__(self):
+        return '\n====================\n Paths:\t\t{0}\n Waypoints:\t{1}\n===================='.format(self.pathsSaved(), self.waypointsSaved())
+
+
+
+class Waypoint(object):
+    # initialization code
+    def __init__(self, lat=0.0, lon=0.0):
+        self.lat = lat
+        self.lon = lon
+
+    # returns the waypoint's assigned latitude
+    def getLat(self):
+        return self.lat
+
+    # returns the waypoint's assigned longitude
+    def getLon(self):
+        return self.lon
+
+    # replaces default identifier when waypoint used in a string
+    def __str__(self):
+        return '[{0}, {1}]'.format(self.lat, self.lon)
+
+
+
+class Path(list):
+    # initialization code
+    def __init__(self):
+        self.waypoints = list()
+
+    # adds a waypoint to the list
+    def addWaypoint(self, wp):
+        self.waypoints.append(wp)
+
+    # removes each instance of a waypoint if it is in path
+    def removeWaypoint(self, wp):
+        self.waypoints = [coord for coord in self.waypoints if coord != wp]
+
+    # returns a lits of the path in pairs
+    def getPathPairs(self):
+        pairs = list()
+
+        last = None
+
+        for wp in self.waypoints:
+            if last != None:
+                pairs.append([last, wp])
+            last = wp
+
+        return pairs
+
+    # replaces the default identifier when list is used in a string
+    def __str__(self):
+        return '{0} Waypoints'.format(len(self.waypoints))
--- Programming Projects/Chapter 11/runGPS.py
+++ Programming Projects/Chapter 11/runGPS.py
@@ -1,10 +1,6 @@
 from sys import exit
 import GPS
 
-# display basic stats about GPS unit
-def displayStatus(myGPS):
-    print('\nPaths:\t\t{0}\nWaypoints:\t{1}'.format(myGPS.pathsSaved(), myGPS.waypointsSaved()))
-
 
 # runs interface
 def gpsUI(myGPS):
@@ -24,7 +20,61 @@ def gpsUI(myGPS):
         name = input('Waypoint Name: ')
         myGPS.saveWaypoint(name)
         print('\n{0} saved as Waypoint \'{1}\''.format(myGPS.waypoints[name], name))
+    elif cmd == 'p':
+        # opens path menu
+        print('\nPaths:')
+        # start a new inner loop that will run until a path command is executed
+        while True:  # breaks out itself
+            print('\nPath Commands:')
+            print('New:\t\t\'n\'')
+            print('Add Point:\t\'a\'')
+            print('Remove Point:\t\'r\'')
+            print('Distance:\t\'d\'')
+            print('Cancel\t\t\'c\'\n')
+            cmd = input('Enter Command: ')
+
+            if cmd == 'c':
+                break
+            elif cmd == 'n':
+                # create a new path
+                path = input('Path Name: ')
+                myGPS.savePath(path)
+                break
+            elif cmd == 'a':
+                # adds a point to a path
+                print('\nPaths:')
+                for pt in myGPS.getPaths():
+                    print(pt)
+
+                path = input('\nPath Name: ')
+                name = input('Waypoint Name: ')
+                # adds waypoint to the path, creates both if neither exists
+                myGPS.addPoint(path, name)
+                break
+            elif cmd == 'r':
+                # removes all instances of a point in a path
+                print('\nPaths:')
+                for pt in myGPS.getPaths():
+                    print(pt)
+
+                path = input('\nPath Name: ')
+                name = input('Waypoint Name: ')
+                # adds waypoint to the path, creates both if neither exists
+                myGPS.removePoint(path, name)
+                break;
+            elif cmd == 'd':
+                # get total distance of path
+                print('\nPaths:')
+                for pt in myGPS.getPaths():
+                    print(pt)
+                name = input('\nPath Name: ')
+                distance = myGPS.pathDistance(name)
+                print('Path {0} is {1} kilometers long.'.format(name, distance))
+                break
+
+
     elif cmd == 'd':
+        # calculates distance from current location to given point
         toPoint = input('\nWaypoint: ')
         try:
             distance = myGPS.distance(toPoint)
@@ -42,7 +92,7 @@ def runGPS():
 
     while True:  # not as bad as it looks, can be killed in interface
         #display current status
-        displayStatus(gipped)
+        print(gipped)
 
         #run through one iteration of user input
         gipped = gpsUI(gipped)
