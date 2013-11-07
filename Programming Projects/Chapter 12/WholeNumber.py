e7906cfc7ecdc6d305f561b436f37efbe15aa7a1
MarcusG <marcus.greathouse@cs.olivetcollege.edu>
2013-11-07 11:51:04 -0500

    Correcting init method, using specific error type

--- Programming Projects/Chapter 12/WholeNumber.py
+++ Programming Projects/Chapter 12/WholeNumber.py
@@ -2,42 +2,42 @@ class WholeNumber(int):
     # initialization class
     def __init__(self, num=0):
         num = int(num)
-        if num <= 0:
-            raise Exception('Negative Number: Whole Numbers must be positive!')
+        if num < 0:
+            ValueError('Whole Numbers must be non-negative!')
         self.value = num
 
     # addition
     def __add__(self, num2):
         if self.value + num2 < 0:
-            raise Exception('Negative Number: Whole Numbers must be positive!')
+            ValueError('Whole Numbers must be non-negative!')
         else:
             return int(self.value + num2)
 
     # subtraction
     def __sub__(self, num2):
         if self.value - num2 < 0:
-            raise Exception('Negative Number: Whole Numbers must be positive!')
+            ValueError('Whole Numbers must be non-negative!')
         else:
             return int(self.value - num2)
 
     # multiplication
     def __mul__(self, num2):
         if self.value * num2 < 0:
-            raise Exception('Negative Number: Whole Numbers must be positive!')
+            ValueError('Whole Numbers must be non-negative!')
         else:
             return int(self.value * num2)
 
     # division
     def __truediv__(self, num2):  # __div__ is a Python 2.X holdover and is useless in 3.0
         if self.value / num2 < 0:
-            raise Exception('Negative Number: Whole Numbers must be positive!')
+            ValueError('Whole Numbers must be non-negative!')
         else:
             return int(self.value / num2)
 
     # floor division
     def __floordiv__(self, num2):
         if self.value // num2 < 0:
-            raise Exception('Negative Number: Whole Numbers must be positive!')
+            ValueError('Whole Numbers must be non-negative!')
         else:
             return int(self.value // num2)
 
