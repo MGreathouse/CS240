de447e0c1b09594f18b89cbb6092a4c3a9b18d3d
MarcusG <marcus.greathouse@cs.olivetcollege.edu>
2013-11-14 11:22:46 -0500

    Adding big room movement in room simulation

--- Final Project/BigRoomTest.py
+++ Final Project/BigRoomTest.py
@@ -1,8 +1,56 @@
 from FinalI import *
+from sys import exit
 
-room = Room()
-print(room)
+# runs an interface 
+def interface(pc):
+    disp = 'You are in a big room.'
+    disp += pc.wallWarnings()
+    disp += pc.checkDirs()
+    # display status display
+    print(disp)
 
-pc = PC(room)
+    # get input
+    cmd = input('? ')
+    if goodInput(cmd):
+        cmd = fullDir(cmd)
+        if cmd == 'Quit':
+            print('Goodbye!')
+            exit()
+        else:
+            print(pc.move(cmd))
+    else:
+        print('I do not understand {}.'.format(cmd))
+    return pc
 
-print(room)
+
+# checks input
+def goodInput(way):
+    way = way.lower()
+    validDirs = ['n','s','e','w', 'q', 'north', 'south', 'east', 'west', 'quit']
+    return way in validDirs
+
+
+# gets the full length version of a direction
+def fullDir(way):
+    way = way.lower()
+    dirDict = {'n':'North', 'north':'North',
+        's':'South', 'south':'South',
+        'e':'East', 'east':'East',
+        'w':'West', 'west':'West',
+        'q':'Quit', 'quit':'Quit'}
+    return dirDict[way]
+
+
+# main function for big room test
+def main():
+    # instantiate intances of room and player
+    room = Room()
+    pc = PC(room)
+
+    while True:
+        pc = interface(pc)
+
+
+# run if this is the file called
+if __name__ == '__main__':
+    main()
--- Final Project/FinalI.py
+++ Final Project/FinalI.py
@@ -1,6 +1,6 @@
 # coordinates are not currently important other than meta knowledge as having to be in a negative
 # range, so for functionality this will use a 0 minimum value instead and just convert the coordinates
-# into the expected values if needed
+# into the expected values if needed [keeping track of desired coords in player class]
 
 class Room(object):
     # initialization method
@@ -24,18 +24,25 @@ class Room(object):
 
             self.floor = floor
             self.center = [length//2, width//2]
+            self.width = width
+            self.length = length
 
     # returns the center coordinate as two integers
     def getCenter(self):
         return self.center[0], self.center[1]
 
-    # add the character into slot
+    # add the character into slot - more for debugging
     def addPC(self, pc):
         w, l = self.getCenter()
         self.floor[w + pc.x][l + pc.y] = pc
 
+    # removes player from the room - more for debugging
+    def removePC(self, pc):
+        w, l = self.getCenter()
+        self.floor[w + pc.x][l + pc.y] = None
+
     # display variables
-    # displays with top left as base: (0,0) in real coords
+    # displays with top left as base: (0,0) in real coords (flipped vertically)
     def __str__(self):  # this is mostly for debugging
         string = ''
         for row in self.floor:
@@ -48,6 +55,7 @@ class Room(object):
         return string
 
 
+# keeping track of desired coordinates in player
 class PC(object):
     # initialization method
     def __init__(self, room, x=0, y=0):
@@ -56,9 +64,103 @@ class PC(object):
         self.room = room
         self.room.addPC(self)
 
+    # check to make sure it is a valid move
+    def checkMove(self, way):
+        realX, realY = self.room.getCenter()
+        realX += self.x
+        realY += self.y
+
+        if way[0].lower() == 'n':
+            if realX < self.room.length - 1:
+                return True
+        elif way[0].lower() == 's':
+            if realX > 0:
+                return True
+        elif way[0].lower() == 'e':
+            if realY < self.room.width - 1:
+                return True
+        elif way[0].lower() == 'w':
+            if realY > 0:
+                return True
+
+        # returns false if those are invalid
+        return False
+
+    # moves player
+    def move(self, way):
+        if self.checkMove(way):
+            # remove the player so the map will update to the fact it is missing
+            self.room.removePC(self)
+
+            if way[0].lower() == 'n':
+                self.x += 1
+            elif way[0].lower() == 's':
+                self.x -= 1
+            elif way[0].lower() == 'e':
+                self.y += 1
+            elif way[0].lower() == 'w':
+                self.y -= 1
+
+            self.room.addPC(self)
+            return 'You travel {} for a bit.'.format(way.lower())
+        else:
+            return 'That way is blocked'
+
     # display methods
+    # string representation of pc
     def __str__(self):
         return 'P'
 
+    # pc repr
     def __repr__(self):
         return 'P: [{}, {}]'.format(self.x, self.y)
+
+    # gets wall warnings
+    def wallWarnings(self):
+        realX, realY = self.room.getCenter()
+        realX += self.x
+        realY += self.y
+
+        # build return string
+        # adjacent walls
+        disp = ''
+        if realX == self.room.length - 1:
+            disp += '\nYou are standing next to a wall to the north.'
+        if realX == 0:
+            disp += '\nYou are standing next to a wall to the south.'
+        if realY == self.room.width - 1:
+            disp += '\nYou are standing next to a wall to the east.'
+        if realY == 0:
+            disp += '\nYou are standing next to a wall to the west.'
+
+        # nearby walls
+        if realX == self.room.length - 2:
+            disp += '\nThere is a wall nearby to the north.'
+        if realX == 1:
+            disp += '\nThere is a wall nearby to the south.'
+        if realY == self.room.width - 2:
+            disp += '\nThere is a wall nearby to the east.'
+        if realY == 1:
+            disp += '\nThere is a wall nearby to the west.'
+
+        return disp
+
+    # get available dirs
+    def checkDirs(self):
+        n = self.checkMove('n')
+        e = self.checkMove('e')
+        s = self.checkMove('s')
+        w = self.checkMove('w')
+
+        disp = '\nObvious directions:'
+        if n:
+            disp += ' n'
+        if s:
+            disp += ' s'
+        if e:
+            disp += ' e'
+        if w:
+            disp += ' w'
+
+        return disp
+
